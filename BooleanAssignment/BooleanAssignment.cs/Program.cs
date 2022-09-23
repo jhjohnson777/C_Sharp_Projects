1664bcdf96cebe82dfa7ae24acdabf0a18eb08c4
jhjohnson777 <105759470+jhjohnson777@users.noreply.github.com>
2022-09-23 10:16:54 -0700

    Boolean assignment initial commit.

---
+++ BooleanAssignment/BooleanAssignment.cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taking a string input and converting it and storing it to an integer variable.
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            // Taking a string input and converting it and storing it to a boolean variable
+            Console.WriteLine("Have you ever had a DUI? Please answer True or False");
+            bool userDUI = Convert.ToBoolean(Console.ReadLine());
+
+            // Taking a string input and converting it and storing it to an integer variable
+            Console.WriteLine("How many speeding tickets do you have?");
+            int userTicket = Convert.ToInt32(Console.ReadLine());
+
+            // Created a boolean variable based on comparing the data provided above and then
+            // printing the response.
+            Console.WriteLine("Qualified?");
+            bool canInsure = userAge >= 15 && userDUI == false && userTicket <= 3;
+            Console.WriteLine(canInsure);
+
+            Console.Read();
+
+        }
+    }
+}
