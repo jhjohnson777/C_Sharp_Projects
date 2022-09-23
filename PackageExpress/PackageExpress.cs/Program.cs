2370a20e49ecfdebe343a782a4420766a124dbb4
jhjohnson777 <105759470+jhjohnson777@users.noreply.github.com>
2022-09-23 13:56:03 -0700

    Changed first ternary to a if else

--- PackageExpress/PackageExpress.cs/Program.cs
+++ PackageExpress/PackageExpress.cs/Program.cs
@@ -16,28 +16,35 @@ namespace PackageExpress.cs
             Console.WriteLine("Please enter the weight on your package.");
             double packWeight = Convert.ToDouble(Console.ReadLine());
 
-            // Based on user input this section uses a tenary statement to flag as overweight and end
+            // Based on user input this section uses a if else statement to flag as overweight and end
             // the program, or ask for more dimensions
-            string weightResult = packWeight > 50 ? "Package too heavy to be shipped via Package Express. Have a good day." :
-                "Please enter the package width?";
-            Console.WriteLine(weightResult);
-            double packWidth = Convert.ToDouble(Console.ReadLine());
+            if (packWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width.");
+                double packWidth = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Please enter the package height.");
-            double packHeight = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the package height.");
+                double packHeight = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Please enter the package length.");
-            double packLength = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the package length.");
+                double packLength = Convert.ToDouble(Console.ReadLine());
 
-            // establishes and recasts variables for total dimensions and shipping costs
-            double totalPack = packWidth + packHeight + packLength;
-            double shipCost = packWidth * packHeight * packLength * packWeight / 100;
-            decimal shippingCost = Convert.ToDecimal(shipCost);
-            
-            // Final tenary statement to show package is oversized or display the shipping cost.
-            string result = totalPack > 50 ? "Package too big to be shipped via Package Express." :
-                "Your estimated total for shipping this package is: $" + shippingCost;
-            Console.WriteLine(result);
+                // establishes and recasts variables for total dimensions and shipping costs
+                double totalPack = packWidth + packHeight + packLength;
+                double shipCost = packWidth * packHeight * packLength * packWeight / 100;
+                decimal shippingCost = Convert.ToDecimal(shipCost);
+
+                // Final tenary statement to show package is oversized or display the shipping cost.
+                string result = totalPack > 50 ? "Package too big to be shipped via Package Express." :
+                    "Your estimated total for shipping this package is: $" + shippingCost;
+                Console.WriteLine(result);
+
+                
+            }
 
             Console.ReadLine();
         }
