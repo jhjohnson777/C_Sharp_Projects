0ffcbe2601b893f4e6279fac6504fed51a7c4c3c
jhjohnson777 <105759470+jhjohnson777@users.noreply.github.com>
2022-09-26 09:21:27 -0700

    While assignment initial

---
+++ WhileAssignment/WhileAssignment.cs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // set var to 0
+            int x = 0;
+            // while x is less or equal to 15 
+            while (x <= 15)
+            {
+                // write x
+                Console.WriteLine(x);
+                // increment x
+                x++;
+            }
+
+            // set var to 1
+            int i = 1;
+            // do this no matter what
+            do
+            {
+                // write i
+                Console.WriteLine(i);
+                // increment i
+                i++;
+            }
+            // while i is less than 10
+            while (i < 10);
+                
+            Console.Read();
+        }
+    }
+}
