7f11ee908fad52fe7d9f3b6cc2edd3d3cdcd137f
jhjohnson777 <105759470+jhjohnson777@users.noreply.github.com>
2022-10-11 14:52:52 -0700

    Calling Methods initial

---
+++ CallingMethods/CallingMethods.cs/Numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods.cs
+{
+    class Numbers
+    {
+        // Public method to add 2 to the users input.
+        public static int AddTwo(int input)
+        {
+            int answer1 = input + 2;
+            return answer1;
+        }
+
+        // Public method to subtract 2 from the users input.
+        public static int SubtractTwo(int input)
+        {
+            int answer2 = input - 2;
+            return answer2;
+        }
+
+        // Public method to multiply users input by 2.
+        public static int TimesTwo(int input)
+        {
+            int answer3 = input * 2;
+            return answer3;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciate new class into the program.
+            Numbers numbers = new Numbers();
+
+            // Ask for user input and convert it to int.
+            Console.WriteLine("Please enter an integer.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            // Prints each method return.
+            Console.WriteLine(Numbers.AddTwo(input));
+            Console.WriteLine(Numbers.SubtractTwo(input));
+            Console.WriteLine(Numbers.TimesTwo(input));
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
